550951077298e25114c9c86d2d5dc0b2816aa96c
Elbahjaoui Jihane <Jihane.Elbahjaoui@emsi-edu.ma>
2022-04-27 11:42:10 +0000

    la 2ème partier de projet patient

--- src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -17,17 +17,17 @@ public class PatientMvcApplication {
         SpringApplication.run(PatientMvcApplication.class, args);
     }
 
-    @Bean
+    //@Bean
    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args -> {
             patientRepository.save(
-                    new Patient(null, "hassan",new Date(),false,12));
+                    new Patient(null, "hassan",new Date(),false,122));
             patientRepository.save(
                     new Patient(null, "Mohamed",new Date(),true,321));
             patientRepository.save(
-                    new Patient(null, "yasmine",new Date(),true,65));
+                    new Patient(null, "yasmine",new Date(),true,165));
             patientRepository.save(
-                    new Patient(null, "hanae",new Date(),false,32));
+                    new Patient(null, "hanae",new Date(),false,132));
 
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
